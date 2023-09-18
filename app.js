f833ad783d22fd05796e3e2b539a40943913cf4a
Hanka8 <hanka.maruskevicova@gmail.com>
2023-09-18 20:59:26 +0200

    authentication working

---
+++ app.js
@@ -0,0 +1,94 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Member = require('./models/member');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+//config dotenv
+dotenv.config();
+
+//create a connection to the database
+mongoose.connect(process.env.MY_KEY, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log("before the passport stuff");
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await Member.findOne({ email: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      };
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+console.log("after the passport stuff");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await Member.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen( process.env.PORT || 3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+module.exports = app;
